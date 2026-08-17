e1e8e6515781085b54db566d91037a48ad10ba70
HungNV <hung@m.ibis.studio>
2026-08-17 16:17:10 +0700

    up

--- src/components/users/UserPagination.tsx
+++ src/components/users/UserPagination.tsx
@@ -1,25 +1,36 @@
+interface UserPaginationProps {
+    currentPage: number;
+    totalPages: number;
+    startIndex: number;
+    onPageChange: (page: number) => void;
+}
 
-const UserPagination = () => {
+
+const UserPagination = ({ currentPage, totalPages, startIndex, onPageChange} : UserPaginationProps) => {
     return (
         <>
             <div className="flex justify-between items-center px-4 py-3">
                 <div className="text-sm text-slate-500">
-                Showing <b>1-5</b> of 45
+                Showing <b>{startIndex + 1}-{Math.min(startIndex + 2, totalPages * 2)}</b> of {totalPages * 2}
                 </div>
                 <div className="flex space-x-1">
-                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                    <button disabled={currentPage === 1} onClick={() => onPageChange(currentPage - 1)} className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                         Prev
                     </button>
-                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
-                        1
-                    </button>
-                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        2
-                    </button>
-                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        3
-                    </button>
-                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                    {Array.from({ length: totalPages }, (_, index) => (
+                        <button 
+                            key={index} 
+                            onClick={() => onPageChange(index + 1)}
+                            className={`px-3 py-1 min-w-9 min-h-9 text-sm font-normal rounded transition duration-200 ease ${
+                                currentPage === index + 1 
+                                    ? 'bg-blue-500 text-white border border-blue-500' 
+                                    : 'text-slate-500 bg-white border border-slate-200 hover:bg-slate-50 hover:border-slate-400'
+                            }`}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                    <button disabled={currentPage === totalPages} onClick={() => onPageChange(currentPage + 1)} className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                         Next
                     </button>
                 </div>
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,18 +1,33 @@
 import UserPagination from "@/components/users/UserPagination";
 import  UsersSearch from "@/components/users/UsersSearch";
 import UserTable from "@/components/users/UserTable";
-import { useState } from "react";
+import { useState,  useEffect } from "react";
 import { users } from  "@/data/users";
 
 
 const Users = () => {
 	const [search, setSearch] = useState('');
+	const [currentPage, setCurrentPage] = useState(1);
+	const pageSize = 2;
+
+	useEffect(() => {
+		setCurrentPage(1);
+	}, [search]);
+
 	const normalizedSearch = search.trim().toLowerCase();
 
 	const filteredUsers = users.filter((user) =>
 		user.name.toLowerCase().includes(normalizedSearch) ||
 		user.email.toLowerCase().includes(normalizedSearch)
 	);
+
+
+	const totalPages = Math.ceil(filteredUsers.length / pageSize);
+	const startIndex = (currentPage - 1) * pageSize;
+	const endIndex = startIndex + pageSize;
+	const currentUsers = filteredUsers.slice(startIndex, endIndex);
+
+
 
 	return (
 		<>
@@ -22,9 +37,9 @@ const Users = () => {
 					<p className="txt-intro text-md mb-5">Manage all users in one place. Control access, assign roles, and monitor activity across your platform.</p>
 					<UsersSearch search={search} setSearch={setSearch}/>
 					<div className="user-table relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default">
-						<UserTable data={filteredUsers} />
+						<UserTable data={currentUsers} />
 					</div>
-					<UserPagination />
+					<UserPagination startIndex={startIndex} currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
 				</div>
 			</section>
 		</>
